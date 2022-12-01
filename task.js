fe3c1ddbc6b8cf16cf90b6d6d9cd0b207157574f
thammaliabhishek <thammaliabhishek05@gmail.com>
2022-12-01 17:07:51 +0530

    one

---
+++ task.js
@@ -0,0 +1,8 @@
+let inputPassword =document.getElementsByClassName('main_input');
+
+
+
+document.addEventListener('click',()=>{
+    let btn = document.getElementsByClassName('btn_input');
+    console.log("")
+})
