92fa820390c443903ef898b78d7f445057fc659b
thammaliabhishek <104211415+thammaliabhishek@users.noreply.github.com>
2022-12-01 17:09:12 +0530

    Delete task.js

--- task.js
+++
@@ -1,8 +0,0 @@
-let inputPassword =document.getElementsByClassName('main_input');
-
-
-
-document.addEventListener('click',()=>{
-    let btn = document.getElementsByClassName('btn_input');
-    console.log("")
-})
